73146f1b9ff86dc33f4247e527c27652ddfcf47f
Benjamin Thorel <thorel.benjamin1@gmail.com>
2024-12-19 13:17:38 +0100

    Rework fonction getMessagesForUserAndSender

--- models/MessageManager.php
+++ models/MessageManager.php
@@ -25,28 +25,34 @@ class MessageManager extends AbstractEntityManager
 
     
     public function getMessagesForUserAndSender($user_id, $senderId): array
-    {
-        $sql = "SELECT m.*, u.username, u.user_logo FROM message m JOIN user u ON m.sender_id = u.user_id WHERE (m.recipient_id = :user_id OR m.recipient_id = :senderId) AND (m.sender_id = :senderId OR m.sender_id = :user_id)";
+{
+    $sql = "SELECT m.*, u.username, u.user_logo 
+            FROM message m 
+            JOIN user u ON m.sender_id = u.user_id 
+            WHERE (m.recipient_id = :user_id OR m.recipient_id = :senderId) 
+              AND (m.sender_id = :senderId OR m.sender_id = :user_id)
+            ORDER BY m.sent_at ASC";  
 
-        $result = $this->db->query($sql, [
-            'user_id' => $user_id,   
-            'senderId' => $senderId
-        ]); 
-        $messages = [];
+    $result = $this->db->query($sql, [
+        'user_id' => $user_id,
+        'senderId' => $senderId
+    ]); 
+    
+    $messages = [];
 
-        while ($message = $result->fetch()) {
-            $msg = new Message($message);
+    while ($message = $result->fetch()) {
+        $msg = new Message($message);
 
-                $date = new DateTime($message['sent_at']);
-                $formattedTime = $date->format('d.m H:i');
+        $date = new DateTime($message['sent_at']);
+        $formattedTime = $date->format('d.m H:i');
 
-                $msg->setFormattedSentAt($formattedTime);
+        $msg->setFormattedSentAt($formattedTime);
 
-                $messages[] = $msg;
-        }
-
-        return $messages;
+        $messages[] = $msg;
     }
+
+    return $messages;
+}
 
     public function addMessage(Message $message) : void
     {
